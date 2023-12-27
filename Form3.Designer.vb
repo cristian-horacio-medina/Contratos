8da0dc9696c6e71a77578f0fc28ee89c2f8d9adf
cristian-horacio-medina <chmedina@gmail.com>
2023-12-27 20:27:12 -0300

    27122023

--- Form3.Designer.vb
+++ Form3.Designer.vb
@@ -23,16 +23,6 @@ Partial Class Form3
     Private Sub InitializeComponent()
         Me.ReportViewer1 = New Microsoft.Reporting.WinForms.ReportViewer()
         Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-        Me.docente = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.carrera = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.horario = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.anio = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.división = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.materia = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.nombredia = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.monbre_abr = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.número_dni = New System.Windows.Forms.DataGridViewTextBoxColumn()
-        Me.sexo = New System.Windows.Forms.DataGridViewTextBoxColumn()
         Me.DataSet1 = New System.Data.DataSet()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -50,62 +40,11 @@ Partial Class Form3
         'DataGridView1
         '
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.docente, Me.carrera, Me.horario, Me.anio, Me.división, Me.materia, Me.nombredia, Me.monbre_abr, Me.número_dni, Me.sexo})
         Me.DataGridView1.Location = New System.Drawing.Point(13, 12)
         Me.DataGridView1.Name = "DataGridView1"
         Me.DataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
         Me.DataGridView1.Size = New System.Drawing.Size(1071, 207)
         Me.DataGridView1.TabIndex = 1
-        '
-        'docente
-        '
-        Me.docente.HeaderText = "docente"
-        Me.docente.Name = "docente"
-        '
-        'carrera
-        '
-        Me.carrera.HeaderText = "carrera"
-        Me.carrera.Name = "carrera"
-        '
-        'horario
-        '
-        Me.horario.HeaderText = "horario"
-        Me.horario.Name = "horario"
-        '
-        'anio
-        '
-        Me.anio.HeaderText = "año"
-        Me.anio.Name = "anio"
-        '
-        'división
-        '
-        Me.división.HeaderText = "división"
-        Me.división.Name = "división"
-        '
-        'materia
-        '
-        Me.materia.HeaderText = "materia"
-        Me.materia.Name = "materia"
-        '
-        'nombredia
-        '
-        Me.nombredia.HeaderText = "día"
-        Me.nombredia.Name = "nombredia"
-        '
-        'monbre_abr
-        '
-        Me.monbre_abr.HeaderText = "tipo_doc"
-        Me.monbre_abr.Name = "monbre_abr"
-        '
-        'número_dni
-        '
-        Me.número_dni.HeaderText = "DNI"
-        Me.número_dni.Name = "número_dni"
-        '
-        'sexo
-        '
-        Me.sexo.HeaderText = "sexo"
-        Me.sexo.Name = "sexo"
         '
         'DataSet1
         '
@@ -128,15 +67,5 @@ Partial Class Form3
 
     Friend WithEvents ReportViewer1 As Microsoft.Reporting.WinForms.ReportViewer
     Friend WithEvents DataGridView1 As DataGridView
-    Friend WithEvents docente As DataGridViewTextBoxColumn
-    Friend WithEvents carrera As DataGridViewTextBoxColumn
-    Friend WithEvents horario As DataGridViewTextBoxColumn
-    Friend WithEvents anio As DataGridViewTextBoxColumn
-    Friend WithEvents división As DataGridViewTextBoxColumn
-    Friend WithEvents materia As DataGridViewTextBoxColumn
-    Friend WithEvents nombredia As DataGridViewTextBoxColumn
-    Friend WithEvents monbre_abr As DataGridViewTextBoxColumn
-    Friend WithEvents número_dni As DataGridViewTextBoxColumn
-    Friend WithEvents sexo As DataGridViewTextBoxColumn
     Friend WithEvents DataSet1 As DataSet
 End Class
--- Form3.vb
+++ Form3.vb
@@ -1,9 +1,55 @@
 ﻿Imports Microsoft.Reporting.WinForms
+Imports Contratos.Form1
 Public Class Form3
     Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Obtén los datos actuales del DataGridView1
+        Dim datos As DataTable = TryCast(DataGridView1.DataSource, DataTable)
 
-        Me.ReportViewer1.RefreshReport()
+        ' Llama al método para cargar los datos en el ReportViewer
+        ActualizarReportViewerConDatos(datos)
     End Sub
+
+    Public Sub AgregarDatosAlDataGridView(data As DataTable)
+        DataGridView1.DataSource = data
+        ' Llama al método para actualizar el ReportViewer con los datos
+        ActualizarReportViewerConDatos(data)
+    End Sub
+    Private Sub ActualizarReportViewerConDatos(data As DataTable)
+        'Verifica si los datos no son nulos
+        If data IsNot Nothing Then
+            ' Mapear datos del DataTable a una lista de DatosReporte
+            Dim listaDatos As New List(Of DatosparaReporte)()
+
+            For Each row As DataRow In data.Rows
+                Dim datos As New DatosparaReporte()
+                datos.docente = row("docente").ToString()
+                datos.carrera = row("carrera").ToString()
+                datos.horario = row("horario").ToString()
+                datos.anio = row("anio").ToString()
+                datos.division = row("division").ToString()
+                datos.materia = row("materia").ToString()
+                datos.nombre_abr = row("nombre_abr").ToString()
+                datos.numdoc = row("numdoc").ToString()
+                datos.sexo = row("sexo").ToString()
+
+                ' ... Continúa con las demás propiedades según la estructura de tu DataTable y DatosReporte
+
+                listaDatos.Add(datos)
+            Next
+
+            ' Reemplaza "DatosReporte" con el nombre de tu ReportDataSource en el informe .rdlc
+            Dim nombreDataSource As String = "DatosReporte"
+            Dim reportDataSource As New ReportDataSource(nombreDataSource, listaDatos)
+
+            ' Limpia los DataSources existentes y agrega el nuevo ReportDataSource al ReportViewer
+            ReportViewer1.LocalReport.DataSources.Clear()
+            ReportViewer1.LocalReport.DataSources.Add(reportDataSource)
+
+            ' Refresca el ReportViewer para mostrar los nuevos datos
+            ReportViewer1.RefreshReport()
+        End If
+    End Sub
+
 
 
 End Class
