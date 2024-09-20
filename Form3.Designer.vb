58cf1409fcf76bb219f30f670cf7011e9a7963d3
cristian-horacio-medina <chmedina@gmail.com>
2024-09-20 16:55:00 -0300

    Traigo cambios esde remoto

--- Form3.Designer.vb
+++ Form3.Designer.vb
@@ -34,12 +34,21 @@ Partial Class Form3
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Button3 = New System.Windows.Forms.Button()
         Me.Label7 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
         Me.SuspendLayout()
         '
         'DataGridView1
         '
+        Me.DataGridView1.AllowUserToOrderColumns = True
         Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
         Me.DataGridView1.Location = New System.Drawing.Point(13, 61)
         Me.DataGridView1.Name = "DataGridView1"
@@ -54,7 +63,7 @@ Partial Class Form3
         'TextBox1
         '
         Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(240, 248)
+        Me.TextBox1.Location = New System.Drawing.Point(220, 31)
         Me.TextBox1.Name = "TextBox1"
         Me.TextBox1.Size = New System.Drawing.Size(65, 22)
         Me.TextBox1.TabIndex = 2
@@ -62,7 +71,7 @@ Partial Class Form3
         'Button1
         '
         Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button1.Location = New System.Drawing.Point(311, 248)
+        Me.Button1.Location = New System.Drawing.Point(291, 31)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(73, 23)
         Me.Button1.TabIndex = 3
@@ -74,7 +83,7 @@ Partial Class Form3
         Me.Label1.AutoSize = True
         Me.Label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(144, 275)
+        Me.Label1.Location = New System.Drawing.Point(124, 58)
         Me.Label1.Name = "Label1"
         Me.Label1.Size = New System.Drawing.Size(57, 18)
         Me.Label1.TabIndex = 4
@@ -85,7 +94,7 @@ Partial Class Form3
         Me.Label2.AutoSize = True
         Me.Label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(34, 248)
+        Me.Label2.Location = New System.Drawing.Point(14, 31)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(104, 18)
         Me.Label2.TabIndex = 5
@@ -96,7 +105,7 @@ Partial Class Form3
         Me.Label3.AutoSize = True
         Me.Label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(144, 249)
+        Me.Label3.Location = New System.Drawing.Point(124, 32)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(85, 18)
         Me.Label3.TabIndex = 6
@@ -105,7 +114,7 @@ Partial Class Form3
         'Button2
         '
         Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.Location = New System.Drawing.Point(678, 245)
+        Me.Button2.Location = New System.Drawing.Point(519, 33)
         Me.Button2.Name = "Button2"
         Me.Button2.Size = New System.Drawing.Size(80, 25)
         Me.Button2.TabIndex = 7
@@ -117,7 +126,7 @@ Partial Class Form3
         Me.Label4.AutoSize = True
         Me.Label4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(549, 247)
+        Me.Label4.Location = New System.Drawing.Point(390, 35)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(101, 18)
         Me.Label4.TabIndex = 8
@@ -128,7 +137,7 @@ Partial Class Form3
         Me.Label5.AutoSize = True
         Me.Label5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label5.Location = New System.Drawing.Point(549, 275)
+        Me.Label5.Location = New System.Drawing.Point(390, 63)
         Me.Label5.Name = "Label5"
         Me.Label5.Size = New System.Drawing.Size(125, 18)
         Me.Label5.TabIndex = 9
@@ -139,7 +148,7 @@ Partial Class Form3
         Me.Label6.AutoSize = True
         Me.Label6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(807, 251)
+        Me.Label6.Location = New System.Drawing.Point(648, 39)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(71, 18)
         Me.Label6.TabIndex = 10
@@ -147,7 +156,7 @@ Partial Class Form3
         '
         'Button3
         '
-        Me.Button3.Location = New System.Drawing.Point(968, 302)
+        Me.Button3.Location = New System.Drawing.Point(1001, 307)
         Me.Button3.Name = "Button3"
         Me.Button3.Size = New System.Drawing.Size(83, 36)
         Me.Button3.TabIndex = 11
@@ -166,27 +175,89 @@ Partial Class Form3
         Me.Label7.Text = "Hacer doble clic en la comisión a cambiar de nombre. Luego clic en el botón cambi" &
     "ar. Para deshacer el cambio, clic en el botón revertir."
         '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Controls.Add(Me.Label5)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.TextBox1)
+        Me.GroupBox1.Location = New System.Drawing.Point(17, 244)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(732, 94)
+        Me.GroupBox1.TabIndex = 13
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Cambio nombre de comisión"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label9)
+        Me.GroupBox2.Controls.Add(Me.Label8)
+        Me.GroupBox2.Controls.Add(Me.Button4)
+        Me.GroupBox2.Controls.Add(Me.TextBox2)
+        Me.GroupBox2.Location = New System.Drawing.Point(764, 251)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(182, 86)
+        Me.GroupBox2.TabIndex = 14
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "¿Contratado?"
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(19, 49)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(48, 20)
+        Me.TextBox2.TabIndex = 0
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(91, 46)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(67, 26)
+        Me.Button4.TabIndex = 1
+        Me.Button4.Text = "cambiar"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(19, 20)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(63, 13)
+        Me.Label8.TabIndex = 2
+        Me.Label8.Text = "Docente_Id"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.Label9.Location = New System.Drawing.Point(106, 20)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(2, 15)
+        Me.Label9.TabIndex = 3
+        '
         'Form3
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1111, 363)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Button3)
-        Me.Controls.Add(Me.Label6)
-        Me.Controls.Add(Me.Label5)
-        Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.Button2)
-        Me.Controls.Add(Me.Label3)
-        Me.Controls.Add(Me.Label2)
-        Me.Controls.Add(Me.Label1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
         Me.Controls.Add(Me.DataGridView1)
         Me.Name = "Form3"
         Me.Text = "Cambiar_nombre_de_comsión"
         CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.DataSet1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -204,4 +275,10 @@ Partial Class Form3
     Friend WithEvents Label6 As Label
     Friend WithEvents Button3 As Button
     Friend WithEvents Label7 As Label
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents Button4 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label8 As Label
 End Class
