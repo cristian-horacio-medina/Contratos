bb842a29f9393d9a0042e31cc4481039b52d787e
Cristian <chmedina@gmail.com>
2024-09-20 16:54:15 -0300

    Update Form3.vb

--- Form3.vb
+++ Form3.vb
@@ -1,4 +1,4 @@
-﻿Imports Microsoft.Reporting.WinForms
+Imports Microsoft.Reporting.WinForms
 Imports Contratos.Form1
 Imports System.Net.Mime.MediaTypeNames
 Imports System.Data.Common
@@ -25,13 +25,16 @@ Public Class Form3
         Dim i As Integer
         i = DataGridView1.CurrentRow.Index
 
-        TextBox1.Text = DataGridView1.Item(4, i).Value()
-        Label1.Text = DataGridView1.Item(4, i).Value()
-        Label3.Text = DataGridView1.Item(2, i).Value()
+        TextBox1.Text = DataGridView1.Item(5, i).Value()
+        Label1.Text = DataGridView1.Item(5, i).Value()
+        Label3.Text = DataGridView1.Item(3, i).Value()
 
         ' Guarda los valores originales de comision_id y division
-        comision_id_original = CInt(DataGridView1.Item(2, i).Value())
-        division_original = DataGridView1.Item(4, i).Value().ToString()
+        comision_id_original = CInt(DataGridView1.Item(3, i).Value())
+        division_original = DataGridView1.Item(5, i).Value().ToString()
+
+        TextBox2.Text = DataGridView1.Item(9, i).Value()
+        Label9.Text = DataGridView1.Item(0, i).Value()
 
     End Sub
 
@@ -153,5 +156,69 @@ Public Class Form3
 
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
         Me.Close()
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+
+        Dim textoOriginal As String = TextBox2.Text
+        Dim contratado As String = "S"
+
+        Dim valorDeDocenteId As Integer
+
+
+        If Not Integer.TryParse(Label9.Text, valorDeDocenteId) Then
+            MessageBox.Show("Por favor seleccione un docente.")
+            Return
+        End If
+
+        ' Asigna el texto modificado de vuelta al TextBox
+        TextBox2.Text = contratado
+        Label9.Text = valorDeDocenteId
+
+
+        ' Leer la cadena de conexión desde el archivo de configuración.
+        Dim connectionString As String = ConfigurationManager.ConnectionStrings("MyConnectionString").ConnectionString
+
+        Dim conexion As New SqlConnection(connectionString)
+
+        Try
+            ' Abre la conexión
+            conexion.Open()
+
+            ' Consulta SQL de actualización con filtro WHERE
+            Dim consulta As String = "UPDATE AL_DOCENTES SET contratado = @contratado WHERE Docente_ID = @docenteId"
+
+            ' Crea un comando SQL con la consulta y la conexión
+            Dim comando As New SqlCommand(consulta, conexion)
+
+            ' Agrega parámetros
+            comando.Parameters.AddWithValue("@contratado", contratado)
+            ' Aquí debes proporcionar el valor correcto de comision_id
+            comando.Parameters.AddWithValue("@docenteId", valorDeDocenteId)
+
+            ' Ejecuta la consulta de actualización
+            comando.ExecuteNonQuery()
+
+            ' Cierra la conexión
+            conexion.Close()
+
+            MessageBox.Show("Se ha modificado correctamente.")
+        Catch ex As Exception
+            MessageBox.Show("Error al modificar la división: " & ex.Message)
+        Finally
+            ' Cierra la conexión si está abierta
+            If conexion.State = ConnectionState.Open Then
+                conexion.Close()
+            End If
+
+        End Try
+
+
+
+        reloaddata()
     End Sub
 End Class
